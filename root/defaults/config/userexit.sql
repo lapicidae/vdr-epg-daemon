1bdf8a2e6658ce3f1a4a9d2e9ffce3147160fd8e
lapicidae <andreas.hemmerle@gmail.com>
2025-09-13 07:20:17 +0200

    plugins: prevent 'Division by 0' error

--- root/defaults/config/userexit.sql
+++ root/defaults/config/userexit.sql
@@ -1,13 +1,18 @@
 CREATE PROCEDURE userexit ()
 BEGIN
-/*
-* fix tvm/tvsp duration = 0
-*/
-update
-  events
-set
-  duration = 1
-where
-  source in('tvm','tvsp') and
-  duration = 0;
+  /*
+  * This procedure is a workaround to fix event durations.
+  * It sets the 'duration' field to 1 for events where it is 0.
+  *
+  * This prevents a "Division by 0" error that can occur in subsequent
+  * processes, such as 'mergeepg'. The issue affects events from
+  * 'tvm', 'tvsp', and 'xmltv' sources.
+  */
+  UPDATE
+    events
+  SET
+    duration = 1
+  WHERE
+    source IN('tvm','tvsp','xmltv')
+    AND duration = 0;
 END
